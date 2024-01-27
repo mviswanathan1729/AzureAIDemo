bb7f2b9cefced1c65f211527827c34e0e82f712d
“Manohar <Manohar.Viswanathan@gmail.com>
2024-01-26 20:51:45 -0500

    chatbot keys added

--- pages/Benefits_Chatbot.py
+++ pages/Benefits_Chatbot.py
@@ -1,13 +1,16 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
+key = st.secrets["Benefits_Chatbot_key"]
+
 components.html(
-    """
+  f"""
     <html>
     <head>
-       
+        <script src="https://cdn.botpress.cloud/webchat/v1/inject.js"></script>
+        <script src="https://mediafiles.botpress.cloud/{key}/webchat/config.js" defer></script>
  </head>
  <body />
  </html>
-    """, height=500
+      """, height=500
 )
--- pages/Claims_Chatbot.py
+++ pages/Claims_Chatbot.py
@@ -1,11 +1,15 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
+key = st.secrets["Claims_Chatbot_key"]
+
 components.html(
-    """
+    f"""
     <html>
     <head>
-          </head>
+        <script src="https://cdn.botpress.cloud/webchat/v1/inject.js"></script>
+        <script src="https://mediafiles.botpress.cloud/{key}/webchat/config.js" defer></script>
+     </head>
  <body />
  </html>
     """, height=500
